3de0c05b5b1f6afd8a5b33ac0b27cc35a592f7ea
Clément H <etudiant@teleporteur.oclock>
2020-11-12 14:02:21 +0100

    vercel test

--- server/index.js
+++ server/index.js
@@ -9,11 +9,18 @@ app.use(helmet()); // help secure Express app by setting HTTP headers
 app.use(morgan('tiny')); // HTTP request logger middleware for node.js with minimal output
 app.use(cors()); // middleware that can be used to enable CORS with various options
 app.use(express.json()); // only accepting json data
+app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'simpleURL - Simplier URL for simplier life by CH'
-    });
+app.get('/:id', (req, res) => {
+    // redirect to given URL
+});
+
+app.post('/url', (req, res) => {
+    // create short URL
+});
+
+app.get('/url/id', (req, res) => {
+    // get a short URL by id
 });
 
 const port = process.env.PORT || 3000;
