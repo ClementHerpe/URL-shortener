bd0aab18539a21e0912eafd3ea9f0d65905f5bc3
Clément Herpe <clementherpe@gmail.com>
2020-11-16 13:25:53 +0100

    config 11

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors()); // middleware that can be used to enable CORS with various opti
 app.use(express.json()); // only accepting json data
 // app.use(express.static(path.join(__dirname, '../my-app/dist'))); not working 
 // app.use(express.static(__dirname+'../my-app/dist')); not working
-app.use(express.static('./dist'));
+app.use(express.static('./my-app/dist'));
 
 const schema = yup.object().shape({ // Define format required
     slug: yup.string().trim().matches(/[\w\-]/i),
