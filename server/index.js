3b841df6feb71d97bb8d7c33fcf153160ee12d9b
Clément Herpe <clementherpe@gmail.com>
2020-11-17 10:31:03 +0100

    cors change

--- server/index.js
+++ server/index.js
@@ -17,7 +17,12 @@ const app = express ();
 
 app.use(helmet()); // help secure Express app by setting HTTP headers
 app.use(morgan('tiny')); // HTTP request logger middleware for node.js with minimal output
-app.use(cors()); // middleware that can be used to enable CORS with various options
+app.use(cors( {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  } )); // middleware that can be used to enable CORS with various options
 app.use(express.json()); // only accepting json data
 // app.use(express.static(path.join(__dirname, '../my-app/dist'))); not working 
 // app.use(express.static(__dirname+'../my-app/dist')); not working
